8484c79fd2260da1be78e0132c4c0cde675c7c56
shimizu311 <64078863+shimizu311@users.noreply.github.com>
2020-05-04 07:44:39 +0900

    message-model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,23 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+<<<<<<< Updated upstream
+=======
+  has_many :messages
+
+>>>>>>> Stashed changes
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.content?
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
